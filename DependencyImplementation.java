e1ac994a7922a783716402babb922f311350a825
nvnouveau <31138880+nvnouveau@users.noreply.github.com>
2017-08-18 10:50:59 -0700

    Update DependencyImplementation.java

--- DependencyImplementation.java
+++ DependencyImplementation.java
@@ -1,5 +1,7 @@
 package rivaltest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -13,24 +15,31 @@ public class DependencyImplementation implements  DependencyProgram {
 /* To store set of installed components */	  
 	 HashSet hs	= new HashSet();
 
+
+@Override
 public void depend(String keys, List<String> values) {
-	/
+	// TODO Auto-generated method stub
 	
 }
 
 
+@Override
 public void install(String program) {
-	
+	// TODO Auto-generated method stub
 	
 }
 
+
+@Override
 public void remove(String program) {
-	
+	// TODO Auto-generated method stub
 	
 }
 
 
+@Override
 public void list() {
+	// TODO Auto-generated method stub
 	
 }
 
@@ -48,6 +57,21 @@ public void read()
 				if (command.equals(String.valueOf(InputComman.LIST))) {
 					list();
 					System.out.println(command);
+					
+					
+					ArrayList<String> ProgramList = new ArrayList<>(Arrays.asList(arr));
+					 ProgramList.remove(command);
+					if (command.equals(String.valueOf(InputComman.DEPEND))) {
+						depend( ProgramList.get(0),  ProgramList);
+					}
+					if (command.equals(String.valueOf(InputComman.INSTALL))) {
+						install( ProgramList.get(0));
+					}
+					if (command.equals(String.valueOf(InputComman.REMOVE))) {
+						remove( ProgramList.get(0));
+					}
+					
+
 }
 			}
 }}
