ee0775e0b74b309254b36e06e9b3d49187cf61c1
nvnouveau <31138880+nvnouveau@users.noreply.github.com>
2017-08-18 10:40:52 -0700

    Update DependencyImplementation.java

--- DependencyImplementation.java
+++ DependencyImplementation.java
@@ -1,5 +1,53 @@
 package rivaltest;
 
-public class DependencyImplementation {
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
 
+public class DependencyImplementation implements  DependencyProgram {
+/* To store dependency mapping */		
+	 HashMap hm = new HashMap();
+	 
+	 
+/* To store set of installed components */	  
+	 HashSet hs	= new HashSet();
+
+public void depend(String keys, List<String> values) {
+	/
+	
 }
+
+
+public void install(String program) {
+	
+	
+}
+
+public void remove(String program) {
+	
+	
+}
+
+
+public void list() {
+	
+}
+
+public void read()
+{
+		// scanner to read input from the keyboard
+		Scanner sc = new Scanner(System.in);
+	
+			System.out.println("input program");
+
+			String input = sc.nextLine();
+			while (!input.equals(String.valueOf(InputComman.END))) {
+				String[] arr = input.split(" ");
+				String command = arr[0];
+				if (command.equals(String.valueOf(InputComman.LIST))) {
+					list();
+					System.out.println(command);
+}
+			}
+}}
